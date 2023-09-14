7f843ee5a5c371c6e742ae76a79f76db7eeea89d
Matthias Wijnsma <matthiasx95@gmail.com>
2023-09-14 20:59:41 +0200

    Add Vec3d.add, Entity.teleportTo, and Entity.getPosition

--- abstracraft-abstractions/src/main/java/tools/redstone/abstracraft/entity/Entity.java
+++ abstracraft-abstractions/src/main/java/tools/redstone/abstracraft/entity/Entity.java
@@ -1,5 +1,6 @@
 package tools.redstone.abstracraft.entity;
 
+import tools.redstone.abstracraft.math.Vec3d;
 import tools.redstone.picasso.usage.Abstraction;
 
 import java.util.UUID;
@@ -19,4 +20,18 @@ public interface Entity extends Abstraction {
      * Discard this entity from the world immediately.
      */
     default void discard() { unimplemented(); }
+
+    /**
+     * Teleport the entity to the specified position.
+     *
+     * @param position The position to teleport the entity to.
+     */
+    default void teleportTo(Vec3d position) { unimplemented(); }
+
+    /**
+     * Gets the position of the entity.
+     *
+     * @return The position of the entity.
+     */
+    default Vec3d getPosition() { return unimplemented(); }
 }
--- abstracraft-math/src/main/java/tools/redstone/abstracraft/math/Vec3d.java
+++ abstracraft-math/src/main/java/tools/redstone/abstracraft/math/Vec3d.java
@@ -1,4 +1,7 @@
 package tools.redstone.abstracraft.math;
 
 public record Vec3d(double x, double y, double z) {
+    public Vec3d add(Vec3d other) {
+        return new Vec3d(x + other.x, y + other.y, z + other.z);
+    }
 }
